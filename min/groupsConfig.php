6b56f1ce0ae84241b6ac15ecbbbb634d5babdb00
Conrad Guelzow <conrad.guelzow@basilicom.de>
2014-05-28 16:54:23 +0200

    yeah

---
+++ min/groupsConfig.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
+
+/** 
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ * See http://code.google.com/p/minify/wiki/CustomSource for other ideas
+ **/
+
+return array(
+    'js-desktop' => array(
+        '//website/static/js/lib/jquery.min.js',
+        '//website/static/js/lib/json3.js',
+        '//website/static/js/lib/modernizr.js',
+        '//website/static/js/site/namespace.js',
+        '//website/static/js/plugins/jquery.marquee.js',
+        '//website/static/js/plugins/jquery.easing.js',
+        '//website/static/js/plugins/jquery.scrollto.js',
+        '//website/static/js/plugins/jquery.selectric.js',
+        '//website/static/js/plugins/jquery.selectize.js',
+        '//website/static/js/plugins/jquery.ui.js',
+        '//website/static/js/plugins/fotorama.js',
+        '//website/static/js/site/app.js',
+        '//website/static/js/site/net.js',
+        '//website/static/js/site/helper.js',
+        '//website/static/js/site/util.js',
+        '//website/static/js/site/forms.js',
+        '//website/static/js/site/search.js'
+    ),
+    'js-mobile' => array(
+        '//website/static/js/lib/jquery.min.js',
+        '//website/static/js/lib/json3.js',
+        '//website/static/js/lib/modernizr.js',
+        '//website/static/js/site/namespace.js',
+        '//website/static/js/plugins/jquery.marquee.js',
+        '//website/static/js/plugins/jquery.easing.js',
+        '//website/static/js/plugins/jquery.scrollto.js',
+        '//website/static/js/plugins/jquery.selectric.js',
+        '//website/static/js/plugins/jquery.selectize.js',
+        '//website/static/js/plugins/jquery.ui.js',
+        '//website/static/js/plugins/fotorama.js',
+        '//website/static/mobile/js/jquery.ui.punch.js',
+        '//website/static/mobile/js/app.js',
+        '//website/static/mobile/js/net.js',
+        '//website/static/mobile/js/helper.js',
+        '//website/static/mobile/js/util.js',
+        '//website/static/mobile/js/forms.js',
+        '//website/static/mobile/js/search.js'
+    ),
+    'css-desktop' => array(
+        '//website/static/css/reset.css',
+        '//website/static/css/fonts.css',
+        '//website/static/css/grid.css',
+        '//website/static/css/layout.css',
+        '//website/static/css/header.css',
+        '//website/static/css/selectbox.css',
+        '//website/static/css/teaser.css',
+        '//website/static/css/forms.css',
+        '//website/static/css/footer.css',
+        '//website/static/css/selectize.css',
+        '//website/static/css/cars.css',
+        '//website/static/css/slider.css',
+        '//website/static/css/overlay.css',
+        '//website/static/css/fotorama.css',
+        '//website/static/css/ui-lightness/jquery.ui.css'
+    ),
+    'css-mobile' => array(
+        '//website/static/css/reset.css',
+        '//website/static/css/fonts.css',
+        '//website/static/mobile/css/grid.css',
+        '//website/static/mobile/css/layout.css',
+        '//website/static/mobile/css/header.css',
+        '//website/static/mobile/css/selectbox.css',
+        '//website/static/mobile/css/teaser.css',
+        '//website/static/mobile/css/forms.css',
+        '//website/static/css/footer.css',
+        '//website/static/mobile/css/selectize.css',
+        '//website/static/mobile/css/cars.css',
+        '//website/static/mobile/css/slider.css',
+        '//website/static/mobile/css/overlay.css',
+        '//website/static/mobile/css/fotorama.css',
+        '//website/static/mobile/css/ui-lightness/jquery.ui.css'
+    )
+);
+
